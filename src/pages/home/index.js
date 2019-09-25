636a8c70ab1266b3b58baed544376cac4b1f56f7
Danny Lin <danny.l@ljit.io>
2019-09-25 18:20:36 +0800

    [Home] Toolbar

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,14 +1,39 @@
 import React from 'react';
 import {
 	Page,
+	Toolbar,
+	ToolbarButton,
+	Icon,
 } from 'react-onsenui';
+import OnsenUi from 'onsenui';
 
 function Home({
 	navigator,
 }) {
+	function _renderToolbar() {
+		return (
+			<Toolbar>
+				<div className="center">
+					Home
+				</div>
+
+				<div className="right">
+					<ToolbarButton onClick={_handleClickWallets}>
+						<Icon icon="fa-wallet" />
+					</ToolbarButton>
+				</div>
+			</Toolbar>
+		);
+	}
+
+	function _handleClickWallets() {
+		OnsenUi.notification.alert('Click wallets');
+	}
+
 	return (
-		<Page>
-			<h1>Home</h1>
+		<Page
+			renderToolbar={_renderToolbar}
+		>
 		</Page>
 	);
 }
